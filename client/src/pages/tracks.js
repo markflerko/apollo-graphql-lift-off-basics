891fc4927131652e7825f78142ec70b3079cd048
markflerko <markflerko@gmail.com>
2022-07-06 00:06:59 +0400

    feat: lift-off 1 basics

---
+++ client/src/pages/tracks.js
@@ -0,0 +1,41 @@
+import { gql, useQuery } from "@apollo/client";
+import React from "react";
+import { Layout, QueryResult } from "../components";
+import TrackCard from "../containers/track-card";
+
+const TRACKS = gql`
+  query ExampleQuery {
+    tracksForHome {
+      id
+      title
+      author {
+        id
+        name
+        photo
+      }
+      thumbnail
+      length
+      modulesCount
+    }
+  }
+`;
+
+/**
+ * Tracks Page is the Catstronauts home page.
+ * We display a grid of tracks fetched with useQuery with the TRACKS query
+ */
+const Tracks = () => {
+  const { loading, error, data } = useQuery(TRACKS);
+
+  return (
+    <Layout grid>
+      <QueryResult loading={loading} error={error} data={data}>
+        {data?.tracksForHome?.map((track) => (
+          <TrackCard key={track?.id} track={track} />
+        ))}
+      </QueryResult>
+    </Layout>
+  );
+};
+
+export default Tracks;
