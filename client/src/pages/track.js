6447da71917e664f41f7bc2fdee8bab6aeb60258
markflerko <markflerko@gmail.com>
2022-07-13 15:45:42 +0400

    feat: 04 mutations

---
+++ client/src/pages/track.js
@@ -0,0 +1,44 @@
+import { gql, useQuery } from "@apollo/client";
+import React from "react";
+import { Layout, QueryResult } from "../components";
+import TrackDetail from "../components/track-detail";
+
+export const GET_TRACK = gql`
+  query getTrack($trackId: ID!) {
+    track(id: $trackId) {
+      id
+      title
+      author {
+        id
+        name
+        photo
+      }
+      description
+      length
+      modules {
+        id
+        length
+        title
+      }
+      modulesCount
+      numberOfViews
+      thumbnail
+    }
+  }
+`;
+
+const Track = ({ trackId }) => {
+  const { loading, error, data } = useQuery(GET_TRACK, {
+    variables: { trackId },
+  });
+
+  return (
+    <Layout>
+      <QueryResult loading={loading} error={error} data={data}>
+        <TrackDetail track={data?.track} />
+      </QueryResult>
+    </Layout>
+  );
+};
+
+export default Track;
